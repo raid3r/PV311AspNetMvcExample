abf77f010ac62e4fac1890904b6bae5b3999b283
Vasyl Kovalov <raid3r81@gmail.com>
2025-02-28 21:11:12 +0200

    Add ChangePasswordForm model to Auth area
    Introduced a new form model for handling password change requests. This includes fields for the current password, new password, and password confirmation. The addition supports future features requiring password update functionality.

---
+++ AspNetMvcExample/Areas/Auth/Models/Forms/ChangePasswordForm.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetMvcExample.Areas.Auth.Models.Forms;
+
+public class ChangePasswordForm
+{
+    public string Password { get; set; }
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+}
