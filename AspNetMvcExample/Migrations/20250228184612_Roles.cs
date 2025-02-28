32aa04b6f9a16af30e44b2e5cfcbcb3ff410d6df
Vasyl Kovalov <raid3r81@gmail.com>
2025-02-28 21:09:52 +0200

    Add Roles migration with seed data for Admin and User roles
    This migration inserts default roles ("Admin" and "User") into the AspNetRoles table to support role-based authorization. The down method is currently left empty for potential future rollback implementation.

---
+++ AspNetMvcExample/Migrations/20250228184612_Roles.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetMvcExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("AspNetRoles", 
+                ["Name", "NormalizedName"] ,
+                new object[] { "Admin", "ADMIN" });
+            
+            migrationBuilder.InsertData("AspNetRoles", 
+                ["Name", "NormalizedName"] ,
+                new object[] { "User", "USER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
