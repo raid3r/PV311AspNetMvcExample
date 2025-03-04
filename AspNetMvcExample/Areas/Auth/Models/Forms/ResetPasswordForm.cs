652f5605865543b505ebf931aab8aa862650156d
Vasyl Kovalov <raid3r81@gmail.com>
2025-03-04 19:37:44 +0200

    Add ResetPasswordForm model to Auth area
    Introduce a ResetPasswordForm class with properties for new and confirmed passwords. This will be used to handle password reset functionality in the Auth area.

---
+++ AspNetMvcExample/Areas/Auth/Models/Forms/ResetPasswordForm.cs
@@ -0,0 +1,7 @@
+﻿namespace AspNetMvcExample.Areas.Auth.Models.Forms;
+
+public class ResetPasswordForm
+{
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+}
